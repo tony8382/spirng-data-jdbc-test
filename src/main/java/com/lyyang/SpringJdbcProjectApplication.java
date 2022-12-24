22c2a6f852fa4ba8cdf55134d063179cde490ec0
Li-Yu, Yang <tony830802@gmail.com>
2022-12-24 16:02:42 +0800

    init

---
+++ src/main/java/com/lyyang/SpringJdbcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lyyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcProjectApplication.class, args);
+	}
+
+}
